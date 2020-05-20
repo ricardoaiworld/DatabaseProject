9ce6651b15fe2bcbc05d96fce225713920eea4d4
OREOCRISPY <bz1188@nyu.edu>
2020-05-20 16:35:07 -0400

    123

--- app_demo/views.py
+++ app_demo/views.py
@@ -68,7 +68,7 @@ def projectdetail(request):
         result = cursor.fetchall()
         return render(request, 'login/projectdetail.html', {'issue_list': result})
     if request.method == 'POST' and 'button1' in request.POST:
-<<<<<<< HEAD
+        pid=request.session['pid']
         sql="insert into issue(uid,pid,title,idscpt,wid,ctime) values(%s,%s,%s,%s,1,NOW())"
         cursor=db.cursor()
         new_issue_title=request.POST['title']
@@ -80,21 +80,20 @@ def projectdetail(request):
         cursor.execute(sql, [pid])
         result = cursor.fetchall()
         return render(request, 'login/projectdetail.html', {'issue_list': result})
-=======
-          sql="insert into issue values(%s,%s,%s,%s,1,now())"
-          cursor=db.cursor()
-          new_issue_title=request.POST['title']
-          new_issue_describe=request.POST.get('describe',False)
-          pid=request.session['pid']
-          cursor.execute(sql,[request.session['user_id'],pid,new_issue_title,new_issue_describe])
-          db.commit()
-          sql = "SELECT iid,dname,title,idscpt,wname,ctime FROM issue NATURAL JOIN workflow NATURAL JOIN user WHERE pid = %s"
-          cursor = db.cursor()
-          cursor.execute(sql, [pid])
-          result = cursor.fetchall()
-          return render(request, 'login/projectdetail.html', {'issue_list': result})
+    if request.method == 'POST' and 'button2' in request.POST:
+        sql="insert into issue values(%s,%s,%s,%s,1,now())"
+        cursor=db.cursor()
+        new_issue_title=request.POST['title']
+        new_issue_describe=request.POST.get('describe',False)
+        pid=request.session['pid']
+        cursor.execute(sql,[request.session['user_id'],pid,new_issue_title,new_issue_describe])
+        db.commit()
+        sql = "SELECT iid,dname,title,idscpt,wname,ctime FROM issue NATURAL JOIN workflow NATURAL JOIN user WHERE pid = %s"
+        cursor = db.cursor()
+        cursor.execute(sql, [pid])
+        result = cursor.fetchall()
+        return render(request, 'login/projectdetail.html', {'issue_list': result})
 
->>>>>>> ee6a151bea43b285c6e78e804e0ec5c33ce1e9c8
 
 
 def issuedetail(request):
