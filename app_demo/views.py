54f387fb636129da1cdd1946b0c73add6a18b941
Ricardo Wang <ricardowang19@gmail.com>
2020-05-21 13:48:23 -0400

    add xss protection

--- app_demo/views.py
+++ app_demo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 import pymysql
-from django.http import HttpResponse
+from django.template.defaultfilters import escape
 
 
 db = pymysql.connect("localhost", "root", "12345678", "Bug_Report")
@@ -77,17 +77,18 @@ def index(request):
 
 def newproject(request):
     if request.method=='POST' and 'button_submit' in request.POST:
+        project_title = escape(request.POST.get('project_title'))
+        project_dscpt = escape(request.POST.get('project_des'))
         sql="insert into project(pname,uid,pdscpt) values(%s,%s,%s)"
         cursor = db.cursor()
-        cursor.execute(sql,[request.POST.get('project_title'),request.session['user_id'],request.POST.get('project_des')])
+        cursor.execute(sql,[project_title, request.session['user_id'],project_dscpt])
         sql="select wid,wname from workflow"
         cursor.execute(sql,[])
         available_workflow=cursor.fetchall()
         db.commit()
-        project_title = request.POST.get('project_title')
         sql="select pid from project where pname=%s"
         cursor.execute(sql,project_title)
-        newprojectId=cursor.fetchone()
+        newprojectId = cursor.fetchone()
         request.session['newprojectId'] =newprojectId
         sql = "insert into flowrelationship values(%s,1,5)"
         cursor.execute(sql, newprojectId)
@@ -105,7 +106,9 @@ def newproject(request):
         cursor.execute(sql, [])
         available_workflow = cursor.fetchall()
         sql = "insert into flowrelationship values(%s,%s,%s)"
-        cursor.execute(sql,[newprojectId,request.POST.get('previousWorkflow'),request.POST.get('next_Workflow')])
+        p_w = escape(request.POST.get('previousWorkflow'))
+        n_w = escape(request.POST.get('next_Workflow'))
+        cursor.execute(sql,[newprojectId,p_w,n_w])
         db.commit()
         sql = "select wid,next_wid from flowrelationship where pid=%s"
         cursor.execute(sql, newprojectId)
@@ -115,7 +118,8 @@ def newproject(request):
         newprojectId=request.session['newprojectId']
         sql="insert into workflow(wname) values(%s)"
         cursor = db.cursor()
-        cursor.execute(sql,request.POST.get('new_workflow'))
+        n_w = escape(request.POST.get('new_workflow'))
+        cursor.execute(sql, n_w)
         db.commit()
         sql = "select wid,wname from workflow"
         cursor.execute(sql, [])
@@ -141,7 +145,7 @@ def projectdetail(request):
         sql="insert into issue(uid,pid,title,idscpt,wid,ctime) values(%s,%s,%s,%s,1,NOW())"
         cursor=db.cursor()
         new_issue_title=request.POST['title']
-        new_issue_describe=request.POST.get('describe',False)
+        new_issue_describe=escape(request.POST.get('describe',False))
         pid = request.session['pid']
         cursor.execute(sql,[request.session['user_id'],pid,new_issue_title,new_issue_describe])
         db.commit()
@@ -151,7 +155,7 @@ def projectdetail(request):
         result = cursor.fetchall()
         return render(request, 'login/projectdetail.html', {'issue_list': result})
     if request.method == 'POST' and 'button2' in request.POST:
-        search_title = request.POST.get('search_title', False)
+        search_title = escape(request.POST.get('search_title', False))
         pid = request.session['pid']
         sql_title = "SELECT iid, dname, title, idscpt, wname,ctime FROM issue NATURAL JOIN USER NATURAL JOIN workflow WHERE pid=%s and title like %s"
         cursor = db.cursor()
@@ -159,7 +163,7 @@ def projectdetail(request):
         issues = cursor.fetchall()
         return render(request, 'login/projectdetail.html', {'issue_list': issues})
     if request.method == 'POST' and 'button3' in request.POST:
-        search_user = request.POST.get('search_name', False)
+        search_user = escape(request.POST.get('search_name', False))
         pid = request.session['pid']
         sql_title = "SELECT iid, dname, title, idscpt, wname,ctime FROM issue NATURAL JOIN USER NATURAL JOIN workflow WHERE pid=%s and dname like %s"
         cursor = db.cursor()
@@ -167,7 +171,7 @@ def projectdetail(request):
         issues = cursor.fetchall()
         return render(request, 'login/projectdetail.html', {'issue_list': issues})
     if request.method == 'POST' and 'button4' in request.POST:
-        time = request.POST.get('Time scope', False)
+        time = escape(request.POST.get('Time scope', False))
         pid = request.session['pid']
         sql_filter_time = "SELECT iid, dname, title, idscpt, wname,ctime FROM issue NATURAL JOIN USER NATURAL JOIN workflow WHERE pid=%s and DATE_SUB(NOW(), INTERVAL %s hour)< ctime"
         cursor = db.cursor()
@@ -185,14 +189,14 @@ def issuedetail(request):
     if request.method=='POST' and 'button1' in request.POST:
         iid=global_iid
         new_title=request.POST['title']
-        new_descr=request.POST.get('IssueDescr',False)
+        new_descr=escape(request.POST.get('IssueDescr',False))
         updatesql="update issue set title=%s,idscpt=%s where iid=%s"
         cursor=db.cursor()
         cursor.execute(updatesql,[new_title,new_descr,iid])
         db.commit()
     if request.method=='POST' and 'button2' in request.POST:
         iid = global_iid
-        new_wid = request.POST.get('next_status', False)
+        new_wid = escape(request.POST.get('next_status', False))
         cursor = db.cursor()
         cursor.execute("SELECT pid,wid FROM issue WHERE iid=%s", [iid])
         result = cursor.fetchone()
@@ -249,8 +253,8 @@ def myproject(request):
         my_project_list = cursor.fetchall()
         return render(request, 'login/myproject.html', {'my_project_list': my_project_list})
     if request.method == 'POST' and 'assign' in request.POST:
-        pid = request.POST.get('project_id', False)
-        uid = request.POST.get('user_id', False)
+        pid = escape(request.POST.get('project_id', False))
+        uid = escape(request.POST.get('user_id', False))
         sql_add_lead = "INSERT INTO `lead`(pid, uid) VALUES (%s, %s)"
         cursor = db.cursor()
         cursor.execute(sql_add_lead, [int(pid), int(uid)])
@@ -264,7 +268,7 @@ def myproject(request):
 
 def myissue(request):
     if request.method == 'GET':
-        pid = request.GET.get('pid')
+        pid = escape(request.GET.get('pid'))
         request.session['pid'] = pid
         sql = "SELECT iid,dname,title,idscpt,wname,ctime FROM issue NATURAL JOIN workflow NATURAL JOIN user WHERE pid = %s"
         cursor = db.cursor()
@@ -272,8 +276,8 @@ def myissue(request):
         result = cursor.fetchall()
         return render(request, 'login/myissue.html', {'issue_list': result})
     if request.method == 'POST' and 'assign' in request.POST:
-        iid = request.POST.get('issue_id', False)
-        uid = request.POST.get('user_id', False)
+        iid = escape(request.POST.get('issue_id', False))
+        uid = escape(request.POST.get('user_id', False))
         pid = request.session['pid']
         sql_add_lead = "INSERT INTO assignment(iid, uid, pid) VALUES (%s, %s, %s)"
         cursor = db.cursor()
